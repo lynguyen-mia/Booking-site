32f6582263b0d27e00ab3cae728ac417d514349d
Ly Nguyen <nguyenthihuongly21@gmail.com>
2023-07-08 23:52:54 +0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import React from "react";
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/search" element={<Search />} />
